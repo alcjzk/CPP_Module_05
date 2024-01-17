c3db1b78093b8e6376e137f2ac038aa4a9bf8646
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-18 01:11:37 +0200

    Impl PresidentialPardonForm

--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 #include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string& target)
+    : AForm("Presidential Pardon Form", 25, 5), _target(target)
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other)
+    : AForm(other), _target(other._target)
+{
+}
+
+void PresidentialPardonForm::execute(const Bureaucrat& executor) const
+{
+    validateExecution(executor);
+    std::cout << _target << " has been pardoned by Zaphod Beeblebrox\n";
+}
