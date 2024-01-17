f2436df42af05acf7b7a0926875f13615a9f0aba
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-18 00:47:28 +0200

    Impl schubbery execute

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "ShrubberyCreationForm.hpp"
 
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target = "Unknown Target") :
@@ -12,5 +13,20 @@ ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other)
 
 void ShrubberyCreationForm::execute(const Bureaucrat& executor) const
 {
-    // TODO:
+    validateExecution(executor);
+    std::fstream file_stream(_target + "_shrubbery", std::fstream::out);
+
+    file_stream <<
+        "                        ,@@@@@@@,"
+        "       ,,,.   ,@@@@@@/@@,  .oo8888o."
+        "    ,&%%&%&&%,@@@@@/@@@@@@,8888\88/8o"
+        "   ,%&\%&&%&&%,@@@\@@@/@@@88\88888/88'"
+        "   %&&%&%&/%&&%@@\@@/ /@@@88888\88888'"
+        "   %&&%/ %&%%&&@@\ V /@@' `88\8 `/88'"
+        "   `&%\ ` /%&'    |.|        \ '|8'"
+        "       |o|        | |         | |"
+        "       |.|        | |         | |"
+        "jgs \\/ ._\//_/__/  ,\_//__\\/.  \_//__/_";
+
+    file_stream.close();
 }
