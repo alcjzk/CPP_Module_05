7b02b19d0d3e1962fa4d46617a2fd19005cb011b
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-18 01:02:13 +0200

    Impl RobotomyRequestForm

--- ex02/RobotomyRequestForm.cpp
+++ ex02/RobotomyRequestForm.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 #include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string& target = "Unknown Target")
+    : AForm("Robotomy Request Form", 72, 45), _target(target)
+{
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& other)
+    : AForm(other), _target(other._target)
+{
+}
+
+void RobotomyRequestForm::execute(const Bureaucrat& executor) const
+{
+    static bool will_exec = false;
+    validateExecution(executor);
+
+    if (will_exec)
+    {
+        std::cout << _target << " has been robotomized successfully\n";
+        will_exec = false;
+    }
+    else
+    {
+        std::cout << "robotomy failed\n";
+        will_exec = true;
+    }
+}
+
