d4448745717b2a844ee8bd3ceabe0705fb3a4746
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-17 19:26:19 +0200

    Added form data members
    - Deleted assignment operators & move constructor due to const members
    - Extended default constructor implementation

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -1,1 +1,10 @@
 #include "Form.hpp"
+
+Form::Form(const std::string& name = "Unnamed Form",
+           const unsigned int sign_require_grade = Bureaucrat::GRADE_LOWEST,
+           const unsigned int execute_require_grade = Bureaucrat::GRADE_LOWEST)
+    : _name(name), _sign_require_grade(sign_require_grade),
+      _execute_require_grade(execute_require_grade)
+{
+    // TODO:
+}
--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -1,13 +1,24 @@
 #pragma once
 
+#include <string>
+#include "Bureaucrat.hpp"
+
 class Form
 {
     public:
         ~Form() = default;
-        Form() = default;
+        Form(const std::string& name = "Unnamed Form",
+             const unsigned int sign_require_grade = Bureaucrat::GRADE_LOWEST,
+             const unsigned int execute_require_grade = Bureaucrat::GRADE_LOWEST);
         Form(const Form&) = default;
-        Form(Form&&) = default;
+        Form(Form&&) = delete;
 
-        Form& operator=(const Form&) = default;
-        Form& operator=(Form&&) = default;
+        Form& operator=(const Form&) = delete;
+        Form& operator=(Form&&) = delete;
+
+    private:
+        const std::string   _name;
+        bool                _is_signed = false;
+        const unsigned int  _sign_require_grade;
+        const unsigned int  _execute_require_grade;
 };
