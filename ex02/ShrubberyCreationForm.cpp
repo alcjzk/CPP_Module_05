3cd34df1ce4ae591662e07e43e804b94defc9490
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-17 23:07:14 +0200

    ShrubberyCreationForm definitions

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -1,1 +1,16 @@
 #include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target = "Unknown Target") :
+    AForm("Shrubbery Creation Form", 145, 137), _target(target)
+{
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other) :
+    AForm(other), _target(other._target)
+{
+}
+
+void ShrubberyCreationForm::execute(const Bureaucrat& executor) const
+{
+    // TODO:
+}
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <string>
+#include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+    public:
+        virtual ~ShrubberyCreationForm() override = default;
+        ShrubberyCreationForm(const std::string& target = "Unknown Target");
+        ShrubberyCreationForm(const ShrubberyCreationForm& other);
+        ShrubberyCreationForm(ShrubberyCreationForm&&) = delete;
+
+        ShrubberyCreationForm& operator=(const ShrubberyCreationForm&) = delete;
+        ShrubberyCreationForm& operator=(ShrubberyCreationForm&&) = delete;
+
+        virtual void execute(const Bureaucrat& executor) const override;
+
+    private:
+        std::string _target;
+};
