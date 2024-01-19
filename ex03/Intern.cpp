4866e0aaac1d047fdbce39f428e356350c1a840a
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-19 13:01:22 +0200

    Implemented intern

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 #include "Intern.hpp"
+
+AForm* Intern::makeForm(const std::string& form_name, const std::string& target) const
+{
+    AForm*  form;
+
+    for (const Entry& entry : map)
+    {
+        if (entry.name == form_name)
+        {
+            form = entry.createForm(target);
+            std::cout << "Intern creates " << form << '\n';
+            return form;
+        }
+    }
+    std::cout << "Intern could not create the form. No form named '" << form_name << "' exists.\n";
+    return nullptr;
+}
+
+AForm* Intern::createShrubberyCreationForm(const std::string& target)
+{
+    return new ShrubberyCreationForm(target);
+}
+
+AForm* Intern::createRobotomyRequestForm(const std::string& target)
+{
+    return new RobotomyRequestForm(target);
+}
+
+AForm* Intern::createPresidentialPardonForm(const std::string& target)
+{
+    return new PresidentialPardonForm(target);
+}
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -1,6 +1,36 @@
 #pragma once
 
+#include <string>
+#include "AForm.hpp"
+
 class Intern
 {
+    public:
+        ~Intern() = default;
+        Intern() noexcept = default;
+        Intern(const Intern&) noexcept = default;
+        Intern(Intern&&) noexcept = default;
 
+        Intern& operator=(const Intern&) noexcept = default;
+        Intern& operator=(Intern&&) noexcept = default;
+
+        AForm*  makeForm(const std::string& form_name, const std::string& target) const;
+
+    private:
+        typedef AForm* (*createForm)(const std::string& target);
+        typedef struct Entry
+        {
+            const char* name;
+            createForm  createForm;
+        } Entry;
+
+        static AForm* createShrubberyCreationForm(const std::string& target);
+        static AForm* createRobotomyRequestForm(const std::string& target);
+        static AForm* createPresidentialPardonForm(const std::string& target);
+
+        static constexpr Entry map[] = {
+            {"Shrubbery Creation Form", &Intern::createShrubberyCreationForm},
+            {"Robotomy Request Form", &Intern::createRobotomyRequestForm},
+            {"Presidential Pardon Form", &Intern::createPresidentialPardonForm},
+        };
 };
